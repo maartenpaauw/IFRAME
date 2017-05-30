00f82e6d72c9c3734056c1835392d63ff917a719
Maarten Paauw <maartenpaauw@gmail.com>
2017-05-30 16:42:08 +0200

    Section 7 - Lecture 97

---
+++ Udemy/The Complete Guide/Section 7/Lecture 97/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container">
+        <v-header></v-header>
+        <hr>
+        <div class="row">
+            <v-servers></v-servers>
+            <v-message></v-message>
+        </div>
+        <hr>
+        <v-footer></v-footer>
+    </div>
+</template>
+
+<script>
+    import Header from './components/Shared/Header.vue'
+    import Servers from './components/Server/Servers.vue'
+    import Message from './components/Server/Message.vue'
+    import Footer from './components/Shared/Footer.vue'
+
+    export default {
+        components: {
+            'v-header': Header,
+            'v-servers': Servers,
+            'v-message': Message,
+            'v-footer': Footer
+        }
+    }
+</script>
+
+<style>
+
+</style>
