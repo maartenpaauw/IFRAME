5d454e7cfd008091a3d84475f6a4227c5251fb26
Maarten Paauw <maartenpaauw@gmail.com>
2017-05-30 16:27:09 +0200

    Section 7 - Lecture 96

--- Udemy/The Complete Guide/Section 7/main/src/Home.vue
+++ Udemy/The Complete Guide/Section 7/main/src/Home.vue
@@ -1,22 +1,15 @@
 <template>
-    <span>
-        <p>Server Status: {{ status }}</p>
-        <hr />
-        <button @click="changeStatus">Change status</button>
-    </span>
+    <div>
+        <app-server-status v-for="server in 5"></app-server-status>
+    </div>
 </template>
 
 <script>
+    import ServerStatus from './ServerStatus.vue';
+
     export default {
-        data () {
-            return {
-                status: 'Critical'
-            }
-        },
-        methods: {
-            changeStatus () {
-                this.status = 'Normal'
-            }
+        components: {
+            'app-server-status': ServerStatus
         }
     }
 </script>
