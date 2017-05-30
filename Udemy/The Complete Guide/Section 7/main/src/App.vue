816b8846b8d60929327b2fa5569cd7bd35cc44de
Maarten Paauw <maartenpaauw@gmail.com>
2017-05-30 16:19:34 +0200

    Section 7 - Lecture 96

---
+++ Udemy/The Complete Guide/Section 7/main/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+    <app-server-status></app-server-status>
+</template>
+
+<script>
+    export default { }
+</script>
+
+<style>
+
+</style>
---
+++ Udemy/The Complete Guide/Section 7/main/src/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <span>
+        <p>Server Status: {{ status }}</p>
+        <hr />
+        <button @click="changeStatus">Change status</button>
+    </span>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                status: 'Critical'
+            }
+        },
+        methods: {
+            changeStatus () {
+                this.status = 'Normal'
+            }
+        }
+    }
+</script>
