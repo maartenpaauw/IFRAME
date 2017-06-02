692b23fe76a0d242028f002319969c5d392f106c
Maarten Paauw <maartenpaauw@gmail.com>
2017-06-03 00:58:34 +0200

    Section 9 - Lecture 126

--- Udemy/The Complete Guide/Section 9/Section Code/src/components/New.vue
+++ Udemy/The Complete Guide/Section 9/Section Code/src/components/New.vue
@@ -1,9 +1,18 @@
 <template>
-    <h3>New quote</h3>
+    <div>
+        <h3>New quote</h3>
+        <button @click="counter++">Increate</button>
+        <p>{{ counter }}</p>
+    </div>
 </template>
 
 <script>
     export default {
+        data () {
+            return {
+                counter: 0
+            }
+        }
     }
 </script>
 
