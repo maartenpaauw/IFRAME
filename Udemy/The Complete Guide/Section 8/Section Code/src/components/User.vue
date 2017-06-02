9fbf8766a524ede1b347bb1e929cb8c543aec858
Maarten Paauw <maartenpaauw@gmail.com>
2017-06-02 16:15:26 +0200

    Section 8 - Lecture 111

--- Udemy/The Complete Guide/Section 8/Section Code/src/components/User.vue
+++ Udemy/The Complete Guide/Section 8/Section Code/src/components/User.vue
@@ -7,7 +7,11 @@
         <hr>
         <div class="row">
             <div class="col-xs-12 col-sm-6">
-                <app-user-detail :name="name" @nameWasReset="name = $event"></app-user-detail>
+                <app-user-detail
+                        :name="name"
+                        @nameWasReset="name = $event"
+                        :resetFn="resetName"
+                ></app-user-detail>
             </div>
             <div class="col-xs-12 col-sm-6">
                 <app-user-edit></app-user-edit>
@@ -33,6 +37,9 @@
         methods: {
             changeName () {
                 this.name = 'Rianne'
+            },
+            resetName() {
+                this.name = 'Maarten'
             }
         }
     }
