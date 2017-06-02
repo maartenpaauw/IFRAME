d5bfa3b494cedd5150384580c3fa3e7758328c3c
Maarten Paauw <maartenpaauw@gmail.com>
2017-06-03 00:55:56 +0200

    Section 9 - Lecture 125

--- Udemy/The Complete Guide/Section 9/Section Code/src/App.vue
+++ Udemy/The Complete Guide/Section 9/Section Code/src/App.vue
@@ -2,11 +2,19 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
-                <quote>
-                    <h2 slot="title">{{ title }}</h2>
-                    <p>A wonderful day!</p>
-                    <p>Also a wonderful day!</p>
-                </quote>
+                <button @click="selected = 'quote'">Quote</button>
+                <button @click="selected = 'author'">Author</button>
+                <button @click="selected = 'new'">New</button>
+                <hr>
+                <p>{{ selected }}</p>
+                <component :is="selected">
+                    <p>Default content</p>
+                </component>
+                <!--<quote>-->
+                    <!--<h2 slot="title">{{ title }}</h2>-->
+                    <!--<p>A wonderful day!</p>-->
+                    <!--<p>Also a wonderful day!</p>-->
+                <!--</quote>-->
             </div>
         </div>
     </div>
@@ -14,15 +22,20 @@
 
 <script>
     import Quote from './components/Quote.vue'
+    import Author from './components/Author.vue'
+    import New from './components/New.vue'
 
     export default {
         data () {
             return {
-                title: 'The Quote'
+                title: 'The Quote',
+                selected: 'quote'
             }
         },
         components: {
-            Quote
+            Quote,
+            Author,
+            New
         }
     }
 </script>
---
+++ Udemy/The Complete Guide/Section 9/Section Code/src/components/New.vue
@@ -0,0 +1,11 @@
+<template>
+    <h3>New quote</h3>
+</template>
+
+<script>
+    export default {
+    }
+</script>
+
+<style>
+</style>
