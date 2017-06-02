683d74a89e097860ed47ce8c1be7e3a6d5509dfd
Maarten Paauw <maartenpaauw@gmail.com>
2017-06-03 01:06:58 +0200

    Exercise 8

---
+++ Udemy/The Complete Guide/Exercise 8/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <br>
+                <button @click="selected = 'app-blue'" class="btn btn-primary">Load Blue Template</button>
+                <button @click="selected = 'app-green'" class="btn btn-success">Load Green Template</button>
+                <button @click="selected = 'app-red'" class="btn btn-danger">Load Red Template</button>
+                <hr>
+                <keep-alive>
+                    <component :is="selected">
+                        <p>This is the content.</p>
+                    </component>
+                </keep-alive>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Blue from './components/Blue.vue';
+    import Green from './components/Green.vue';
+    import Red from './components/Red.vue';
+
+    export default {
+        data () {
+            return {
+                selected: 'app-blue'
+            }
+        },
+        components: {
+            appBlue: Blue,
+            appGreen: Green,
+            appRed: Red
+        }
+    }
+</script>
+
+<style>
+</style>
