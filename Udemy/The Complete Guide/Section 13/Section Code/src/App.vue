11ccbf515890c7a78e9e0969becb9f184d0d97f9
Maarten Paauw <maartenpaauw@gmail.com>
2017-06-03 21:43:46 +0200

    Section 13 - Lecture 170

--- Udemy/The Complete Guide/Section 13/Section Code/src/App.vue
+++ Udemy/The Complete Guide/Section 13/Section Code/src/App.vue
@@ -3,7 +3,8 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Filters & Mixins</h1>
-
+                <p>{{ text | uppercase }}</p>
+                <p>{{ text | lowercase }}</p>
             </div>
         </div>
     </div>
@@ -11,6 +12,16 @@
 
 <script>
     export default {
+        data () {
+            return {
+                text: 'Hello There!'
+            }
+        },
+        filters: {
+            uppercase (value) {
+                return value.toUpperCase();
+            }
+        }
     }
 </script>
 
