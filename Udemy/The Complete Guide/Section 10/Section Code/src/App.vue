6ae39cd9f223d2363e240b3ad16320e243e5484c
Maarten Paauw <maartenpaauw@gmail.com>
2017-06-03 11:20:19 +0200

    Section 10 - Lecture 137

--- Udemy/The Complete Guide/Section 10/Section Code/src/App.vue
+++ Udemy/The Complete Guide/Section 10/Section Code/src/App.vue
@@ -1,11 +1,13 @@
 <template>
     <div class="container">
+        <new-quote @quoteAdded="newQuote"></new-quote>
         <quote-grid :quotes="quotes"></quote-grid>
     </div>
 </template>
 
 <script>
     import QuoteGrid from './components/QuoteGrid.vue';
+    import NewQuote from './components/NewQuote.vue';
 
     export default {
         data () {
@@ -17,7 +19,13 @@
             }
         },
         components: {
-            QuoteGrid
+            QuoteGrid,
+            NewQuote
+        },
+        methods: {
+            newQuote (quote) {
+                this.quotes.push(quote);
+            }
         }
     }
 </script>
