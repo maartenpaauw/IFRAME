9cd6059d561f1ddbc9b802506a44f191fbc91e59
Maarten Paauw <maartenpaauw@gmail.com>
2017-06-04 13:28:44 +0200

    Section 14 - Lecture 198

--- Udemy/The Complete Guide/Section 14/Section Code/src/App.vue
+++ Udemy/The Complete Guide/Section 14/Section Code/src/App.vue
@@ -48,19 +48,34 @@
                             :css="false">
                     <div style="width: 300px; height: 100px; background-color: lightgreen;" v-if="load"></div>
                 </transition>
+
+                <hr>
+                <button class="btn btn-primary"
+                        @click="(selectedComponent == 'app-success-alert') ? selectedComponent = 'app-danger-alert' : selectedComponent = 'app-success-alert'">
+                    Toggle components
+                </button>
+                <br>
+                <br>
+                <transition name="fade" mode="out-in">
+                    <component :is="selectedComponent"></component>
+                </transition>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+    import DangerAlert from './DangerAlert.vue';
+    import SuccessAlert from './SuccessAlert.vue';
+
     export default {
         data() {
             return {
                 show: false,
                 load: true,
                 alertAnimation: 'fade',
-                elementWidth: 100
+                elementWidth: 100,
+                selectedComponent: 'app-success-alert'
             }
         },
         methods: {
@@ -112,6 +127,10 @@
             leaveCancelled (el) {
                 console.log('Leave cancelled');
             }
+        },
+        components: {
+            appDangerAlert: DangerAlert,
+            appSuccessAlert: SuccessAlert
         }
     }
 </script>
