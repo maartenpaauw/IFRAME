b225006fdb7f4192bbf493bb73758f06bf5cee8f
Maarten Paauw <maartenpaauw@gmail.com>
2017-06-04 13:47:11 +0200

    Section 14 - Lecture 201

--- Udemy/The Complete Guide/Section 14/Section Code/src/App.vue
+++ Udemy/The Complete Guide/Section 14/Section Code/src/App.vue
@@ -68,13 +68,14 @@
 
                 <br><br>
                 <ul class="list-group">
-                    <li class="list-group-item"
-                        v-for="(number, index) in numbers"
-                        @click="removeItem(index)"
-                        style="cursor: pointer;"
-                    >
-                        {{ number }}
-                    </li>
+                    <transition-group name="slide">
+                        <li class="list-group-item"
+                            v-for="(number, index) in numbers"
+                            @click="removeItem(index)"
+                            style="cursor: pointer;"
+                            :key="number"
+                        >{{ number }}</li>
+                    </transition-group>
                 </ul>
             </div>
         </div>
@@ -194,6 +195,11 @@
         animation: slide-out 1s ease-out forwards;
         transition: opacity 1s;
         opacity: 0;
+        position: absolute;
+    }
+
+    .slide-move {
+        transition: transform 1s;
     }
 
     @keyframes slide-in {
