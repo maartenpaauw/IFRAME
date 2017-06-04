c30a5a107bf32dedb86a4e98a007e0313dd6b79f
Maarten Paauw <maartenpaauw@gmail.com>
2017-06-04 13:13:33 +0200

    Section 14 - Lecture 196

--- Udemy/The Complete Guide/Section 14/Section Code/src/App.vue
+++ Udemy/The Complete Guide/Section 14/Section Code/src/App.vue
@@ -40,10 +40,12 @@
                             @enter="enter"
                             @after-enter="afterEnter"
                             @enter-cancelled="enterCancelled"
+
                             @before-leave="beforeLeave"
                             @leave="leave"
                             @after-leave="afterLeave"
-                            @leave-cancelled="leaveCancelled">
+                            @leave-cancelled="leaveCancelled"
+                            :css="false">
                     <div style="width: 100px; height: 100px; background-color: lightgreen;" v-if="load"></div>
                 </transition>
             </div>
