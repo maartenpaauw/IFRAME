4f3fd6ecc8f9f5b9bc73ab814af5230a0f88deee
Maarten Paauw <maartenpaauw@gmail.com>
2017-06-04 12:02:45 +0200

    Section 14 - Lecture 182

--- Udemy/The Complete Guide/Section 14/Section Code/src/App.vue
+++ Udemy/The Complete Guide/Section 14/Section Code/src/App.vue
@@ -3,7 +3,14 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Animations</h1>
-
+                <hr>
+                <button class="btn btn-primary" @click="show = !show">Show alert</button>
+                <br>
+                <br>
+                <br>
+                <transition>
+                    <div class="alert alert-info" v-if="show">This is an alert.</div>
+                </transition>
             </div>
         </div>
     </div>
@@ -13,7 +20,7 @@
     export default {
         data() {
             return {
-
+                show: true
             }
         }
     }
