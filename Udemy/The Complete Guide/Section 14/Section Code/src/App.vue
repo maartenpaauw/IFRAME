facaf29579c6fab6e0e3f1067a44f1cc4dd81b95
Maarten Paauw <maartenpaauw@gmail.com>
2017-06-04 12:31:47 +0200

    Section 14 - Lecture 191

--- Udemy/The Complete Guide/Section 14/Section Code/src/App.vue
+++ Udemy/The Complete Guide/Section 14/Section Code/src/App.vue
@@ -4,9 +4,14 @@
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Animations</h1>
                 <hr>
+                <select v-model="alertAnimation">
+                    <option value="slide">Slide</option>
+                    <option value="fade">Fade</option>
+                </select>
+                <hr>
                 <button class="btn btn-primary" @click="show = !show">Show alert</button>
                 <hr>
-                <transition name="fade">
+                <transition :name="alertAnimation">
                     <div class="alert alert-info" v-show="show">This is an alert.</div>
                 </transition>
 
@@ -26,7 +31,8 @@
     export default {
         data() {
             return {
-                show: true
+                show: true,
+                alertAnimation: 'fade'
             }
         }
     }
